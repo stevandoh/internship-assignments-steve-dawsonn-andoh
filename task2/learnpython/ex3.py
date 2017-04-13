074da3010fc435750d8573da44f12ec7592db805
stevandoh <stevandoh@gmail.com>
2017-04-13 20:13:31 +0000

    study drills done

--- task2/learnpython/ex3.py
+++ task2/learnpython/ex3.py
@@ -1,17 +1,25 @@
+#display something
 print "I will not count my chickens:"
 
+# divide then sum
 print "Hens", 25 + 30 / 6
+
+# multiple , calculate reminder and substract it from 100
 print "Roosters", 100 -25 * 3 % 4
 
+# display
 print "Now I will count the eggs:"
 
+# 1/5 = 0 + 5 % 2  + 6 => 1 + = 7 
 print 3 + 2 + 1 - 5 + 4 % 2 -1 /5 +6
 print "Is it true that 3 + 2 < 5 -7?"
 
+# boolean value so either True or False
 print 3 + 2 > 5 -7
 
 print "Oh, that's why it's False."
 
+#Boolean
 print "Is it greater?", 5 > -2 
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
