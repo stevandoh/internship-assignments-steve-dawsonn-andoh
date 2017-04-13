02e76ac1ecab18f9cb9c3eb427229d818d25304d
stevandoh <stevandoh@gmail.com>
2017-04-13 20:23:58 +0000

    ex4 study drill completed

--- task2/learnpython/ex4.py
+++ task2/learnpython/ex4.py
@@ -1,10 +1,18 @@
+# car number
 cars = 100
+#number of seat available
 space_in_car = 4.0
+# number of drivers
 drivers = 30
+# number of passengers
 passengers = 90
+#number of cars not driver
 cars_not_driven = cars - drivers
+# if there is a driver then a car is driven
 cars_driven = drivers
+#number of seat * the number of car driven
 carpool_capacity = cars_driven * space_in_car
+# average passengers per car
 average_passengers_per_car = passengers / cars_driven
 
 print "There are ", cars , "cars available."
