f11622a3edaa71b36ac1c57d82961400de086aa6
stevandoh <stevandoh@gmail.com>
2017-04-19 18:22:42 +0000

    static path and url set

--- task3/djangoApp/project/project/settings.py
+++ task3/djangoApp/project/project/settings.py
@@ -69,6 +69,8 @@ TEMPLATES = [
 ]
 
 
+
+
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
@@ -119,4 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'conf', 'static'),
+)
