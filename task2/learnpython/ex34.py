24489cef44bd978c84e4151135e41ea0493daf86
stevandoh <stevandoh@gmail.com>
2017-04-20 16:27:15 +0000

    ex34 done

---
+++ task2/learnpython/ex34.py
@@ -0,0 +1,5 @@
+animals = ['bears', 'python','peacock', 'kangaroo','whale','platypus']
+
+for i in range(len(animals)):
+    print "the ",i+1 ,"animal is at position ",i, "and is a ", animals[i]
+    print "the animal at ", i , "is the ",i+1,"and is a ", animals[i]
